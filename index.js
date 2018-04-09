5423d1555109da02128c061ce00c161fd21d7b76
Ruchi Bahety <ruchi.malpani@gmail.com>
2018-04-09 18:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-  if(num1>=num2)
-    return num1-num2
-  else
-    return num2-num1
+  return num1-num2
 }
 
 function multiply(num1, num2) {
